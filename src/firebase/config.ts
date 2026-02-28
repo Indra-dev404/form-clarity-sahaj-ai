9e70d8fcf49e399051c151daa4fd70a661eaf8bb
Indrajit Biswas <indrajitbiswaspersonal@outlook.com>
2026-02-28 18:41:10 +0530

    Replace Firebase config with placeholder values
    Updated Firebase configuration with placeholder values.

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "studio-8550718039-84d20",
-  "appId": "1:566884455461:web:1426b4b3f4d5520ae25bcc",
-  "apiKey": "AIzaSyCc7B26oxnoE0N8_SPA2kBp_LSBW-NgLQY",
-  "authDomain": "studio-8550718039-84d20.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "566884455461"
+  "projectId": "PROJECT_ID",
+  "appId": "APP_ID",
+  "apiKey": "API_KEY_HERE",
+  "authDomain": "AUTH_ID",
+  "measurementId": "ID",
+  "messagingSenderId": "ID"
 };
